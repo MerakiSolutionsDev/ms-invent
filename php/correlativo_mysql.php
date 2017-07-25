06df6240f8b4fab9e95c396b66c6080dbb9ba20f
Gustavo <galarcon@merakisolutionsdev.com>
2017-07-24 23:09:47 -0500

    new php correlativo: Get the last transaction number.

---
+++ php/correlativo_mysql.php
@@ -0,0 +1,34 @@
+<?php
+    SESSION_START();
+    header("Access-Control-Allow-Origin: *");
+    if(!isset($_SESSION['db'])){
+        header("Location: http://www.merakisolutionsdev.com/rent/ms-invent/html/login.html");
+        exit();
+    }
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "1nj7TkF94h";
+    $dbname = $_SESSION['db'];
+    $counter = 0;
+
+    $connect = mysqli_connect($servername, $username, $password, $dbname);
+
+    if (!$connect) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    $query1 = "SELECT id_transaction FROM `regdocumentos` ORDER BY id DESC LIMIT 1";
+
+    $result = mysqli_query($connect,$query1);
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        $id_transaction = $row['id_transaction'];
+    }
+
+    $id_transaction = intval($id_transaction) + 1;
+
+    $outp .= '{"Operacion":"' . $row['id_transaction'] . '"}';
+
+    echo $out;
+?>
